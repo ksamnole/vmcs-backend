a7a3f3139bd3198a5683b34742f82f2200811d2a
Maksim Tsvetkov <atm.tsvetk@gmail.com>
2022-11-21 02:37:50 +0500

    Add Exception Middleware

---
+++ src/VMCS.API/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace VMCS.API.Middlewares
+{
+    public class ExceptionMiddleware
+    {
+        public readonly RequestDelegate next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            try
+            {
+                await next(httpContext);
+            }
+            catch (ValidationException exception)
+            {
+                httpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+                await httpContext.Response.WriteAsJsonAsync(new { Message = exception.Message, Value = exception.Value });
+            }
+            catch (FluentValidation.ValidationException exception)
+            {
+                var errors = exception.Errors.Select(x => $"{ x.PropertyName }: { x.ErrorMessage }");
+
+                var errorMessage = string.Join(Environment.NewLine, errors);
+                
+                await httpContext.Response.WriteAsJsonAsync(new { Message = errorMessage });
+            }
+            catch (Exception exception)
+            {
+                httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                await httpContext.Response.WriteAsJsonAsync(new { Message = "Внутренняя ошибка сервера" });
+            }
+        }
+    }
+}
