81d6694f066c70184e84c9a2ba9b9dd4fef076db
Maksim Tsvetkov <atm.tsvetk@gmail.com>
2022-11-21 02:38:12 +0500

    Create Core project

---
+++ src/VMCS.Core/Bootstrap.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace VMCS.Core;
+
+public static class Bootstrap
+{
+    public static IServiceCollection AddCore(this IServiceCollection services)
+    {
+        return services;
+    }
+}
