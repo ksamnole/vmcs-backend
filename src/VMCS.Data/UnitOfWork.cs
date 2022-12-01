6850165b833e580f3ee48730cdc39e4679973bd0
KsamNole <atm.tsvetk@gmail.com>
2022-12-02 02:15:37 +0500

    Add UnitOfWork

---
+++ src/VMCS.Data/UnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using VMCS.Core;
+using VMCS.Data.Contexts;
+
+namespace VMCS.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationContext _applicationContext;
+
+        public UnitOfWork(ApplicationContext applicationContext)
+        {
+            _applicationContext = applicationContext;
+        }
+
+        public Task<int> SaveChange()
+        {
+            return _applicationContext.SaveChangesAsync();
+        }
+    }
+}
