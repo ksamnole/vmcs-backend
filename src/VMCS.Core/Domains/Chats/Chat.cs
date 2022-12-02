c7ba75ffb53a54e8ea80d6d57fbce52c79dda87e
batyadmx <andronovaleksandrptg@yandex.ru>
2022-12-02 19:17:47 +0500

    Refactor

--- src/VMCS.Core/Domains/Chats/Chat.cs
+++ src/VMCS.Core/Domains/Chats/Chat.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace VMCS.Core.Domains.Chats
+﻿namespace VMCS.Core.Domains.Chats
 {
-    internal class Chat
+    public class Chat
     {
+        public string Id { get; set; } =  Guid.NewGuid().ToString();
     }
 }
--- src/VMCS.Core/Domains/Messages/Message.cs
+++ src/VMCS.Core/Domains/Messages/Message.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VMCS.Core.Domains.Chats;
+using VMCS.Core.Domains.Users;
 
 namespace VMCS.Core.Domains.Messages
 {
-    internal class Message
+    public class Message
     {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string UserId { get; set; }
+        public User User { get; set; }
+        public string Text { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public string ChatId { get; set; }
+        public Chat Chat {get;set;}
     }
 }
