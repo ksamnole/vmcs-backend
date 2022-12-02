2dfa8dcf4266d4bb1d23e482621b6e9048cb696d
KsamNole <atm.tsvetk@gmail.com>
2022-12-02 16:23:46 +0500

    Add channel name length validation

--- src/VMCS.Core/Domains/Channels/Validators/ChannelValidator.cs
+++ src/VMCS.Core/Domains/Channels/Validators/ChannelValidator.cs
@@ -7,6 +7,7 @@ public class ChannelValidator : AbstractValidator<Channel>
     public ChannelValidator()
     {
         RuleFor(x => x.Name).NotEmpty()
-            .WithMessage("Please specify a Name").Length(3, 20);
+            .WithMessage("Please specify a Name").Length(3, 20)
+                .WithMessage("Name length from 5 to 20 characters");
     }
 }
