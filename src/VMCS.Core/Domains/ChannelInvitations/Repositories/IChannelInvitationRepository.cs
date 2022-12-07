214ffec8dc8de342390f75d5d859bac84a80bc1b
KsamNole <atm.tsvetk@gmail.com>
2022-12-07 18:44:00 +0500

    Create channel invitation repository

---
+++ src/VMCS.Core/Domains/ChannelInvitations/Repositories/IChannelInvitationRepository.cs
@@ -0,0 +1,9 @@
+﻿using VMCS.Core.Domains.Channels;
+
+namespace VMCS.Core.Domains.ChannelInvitations.Repositories;
+
+public interface IChannelInvitationRepository
+{
+    Task Create(ChannelInvitation channelInvitation, CancellationToken cancellationToken);
+    Task Delete(string id, CancellationToken cancellationToken);
+}
---
+++ src/VMCS.Data/ChannelInvitations/Repositories/ChannelInvitationRepository.cs
@@ -0,0 +1,32 @@
+﻿using System.Data.Entity.Core;
+using Microsoft.EntityFrameworkCore;
+using VMCS.Core.Domains.ChannelInvitations.Repositories;
+using VMCS.Core.Domains.Channels;
+using VMCS.Data.Contexts;
+
+namespace VMCS.Data.ChannelInvitations.Repositories;
+
+public class ChannelInvitationRepository : IChannelInvitationRepository
+{
+    private readonly ApplicationContext _applicationContext;
+
+    public ChannelInvitationRepository(ApplicationContext applicationContext)
+    {
+        _applicationContext = applicationContext;
+    }
+
+    public async Task Create(ChannelInvitation channelInvitation, CancellationToken cancellationToken)
+    {
+        await _applicationContext.ChannelInvitations.AddAsync(channelInvitation, cancellationToken);
+    }
+
+    public async Task Delete(string id, CancellationToken cancellationToken)
+    {
+        var entity = await _applicationContext.ChannelInvitations.FirstOrDefaultAsync(x => x.Id == id, cancellationToken);
+
+        if (entity == null)
+            throw new ObjectNotFoundException($"Invitation to channel with Id = {id} not found");
+
+        _applicationContext.ChannelInvitations.Remove(entity);
+    }
+}
