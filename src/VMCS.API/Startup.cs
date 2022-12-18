bd75570860047063b09602d87e5d5ae715da508b
KsamNole <atm.tsvetk@gmail.com>
2022-12-18 20:33:29 +0500

    Authorize to swagger

--- src/VMCS.API/Startup.cs
+++ src/VMCS.API/Startup.cs
@@ -45,6 +45,25 @@ namespace VMCS.API
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "VMCS.API", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme {
+                    In = ParameterLocation.Header, 
+                    Description = "Please insert JWT with Bearer into field",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey 
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                    { 
+                        new OpenApiSecurityScheme 
+                        { 
+                            Reference = new OpenApiReference 
+                            { 
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer" 
+                            } 
+                        },
+                        new string[] { } 
+                    } 
+                });
             });
             
             services.AddIdentity<AuthUser, IdentityRole>(opt =>
