9a098594397c98996f120c8023004f90eebf2e57
Alexander Konovalov <alexxonov@gmail.com>
2023-01-27 09:10:48 +0500

    Update upload avatar for channel

---
+++ src/VMCS.API/Controllers/Channels/Dto/UploadAvatarChannelDTO.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace VMCS.API.Controllers.Channels.Dto
+{
+    public class UploadAvatarChannelDTO
+    {
+        public string ChannelId { get; set; }
+        public IFormFile Image { get; set; }
+    }
+}
